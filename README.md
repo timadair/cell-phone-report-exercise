9804014815f01800d4028865d7246a463d577865
Tim Adair <tymmo.t@gmail.com>
2021-07-31 11:28:11 -0600

    Print test PDF file
    Start with a visible result.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cell-phone-report-exercise
+
+##Usage
+
+
+##Libraries
+**Apache PDFBox**  
+My local printer, an HP OfficeJet 5252, is unable to handle PDF format using Java's PrintJob. 
+PDFBox worked for my local printer, and is more likely to work for more printers than anything I'd do in an hour.
+ 
--- src/main/java/com/timadair/CellPhoneUsageReportApp.java
+++ src/main/java/com/timadair/CellPhoneUsageReportApp.java
@@ -1,7 +1,36 @@
 package com.timadair;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.printing.PDFPageable;
+
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import static java.nio.file.Files.newInputStream;
+
 public class CellPhoneUsageReportApp {
+
+  public static final Path SAMPLE_PDF_PATH = Paths.get("./src/main/resources/pdf-sample.pdf");
+
   public static void main(String[] args) {
-    System.out.println("Running Cell Phone Usage Reporting App");
+    PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
+    PrinterJob job = PrinterJob.getPrinterJob();
+
+    try (InputStream fileStream = newInputStream(SAMPLE_PDF_PATH, StandardOpenOption.READ)) {
+      PDDocument document = PDDocument.load(fileStream);
+      job.setPageable(new PDFPageable(document));
+      job.setPrintService(printService);
+      job.print();
+    } catch (PrinterException | IOException ex) {
+      ex.printStackTrace();
+    }
+
   }
 }
