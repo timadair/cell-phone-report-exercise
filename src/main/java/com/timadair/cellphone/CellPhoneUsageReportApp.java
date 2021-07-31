8d5c7f3d00c2143782994676b329bb9413664681
Tim Adair <tymmo.t@gmail.com>
2021-07-31 15:59:17 -0600

    Resume printing report

--- src/main/java/com/timadair/cellphone/CellPhoneUsageReportApp.java
+++ src/main/java/com/timadair/cellphone/CellPhoneUsageReportApp.java
@@ -1,30 +1,19 @@
 package com.timadair.cellphone;
 
 import com.timadair.cellphone.data.CellPhone;
-import com.timadair.cellphone.data.EmployeeUsageSummary;
 import com.timadair.cellphone.data.PhoneUsageReportData;
 import com.timadair.cellphone.data.UsageEntry;
 import com.timadair.cellphone.service.DataProcessingService;
 import com.timadair.cellphone.service.DataService;
 import com.timadair.cellphone.service.PDFService;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.printing.PDFPageable;
+import com.timadair.cellphone.service.PrintService;
 
-import javax.print.PrintService;
-import javax.print.PrintServiceLookup;
-import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
-import java.util.Map;
-
-import static java.nio.file.Files.newInputStream;
 
 public class CellPhoneUsageReportApp {
 
@@ -52,6 +41,7 @@ public class CellPhoneUsageReportApp {
     DataService dataService = new DataService();
     PDFService pdfService = new PDFService();
     DataProcessingService dataProcessingService = new DataProcessingService();
+    PrintService printService = new PrintService();
 
     List<CellPhone> employeePhones = dataService.getCellPhoneRecords(cellPhoneCsvPath);
     List<UsageEntry> usageEntries = dataService.getUsageRecords(cellPhoneUsageCsvPath);
@@ -60,21 +50,7 @@ public class CellPhoneUsageReportApp {
 
     pdfService.renderMonthlyCellPhoneUsageReport(reportData, REPORT_FILE_DEST, reportStartDate, reportEndDate, monthsCovered);
 
-//    printPDFFile(Paths.get(REPORT_FILE_DEST));
-//    Files.delete(Paths.get(REPORT_FILE_DEST));
-  }
-
-  private static void printPDFFile(Path samplePdfPath) {
-    PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
-    PrinterJob job = PrinterJob.getPrinterJob();
-
-    try (InputStream fileStream = newInputStream(samplePdfPath, StandardOpenOption.READ)) {
-      PDDocument document = PDDocument.load(fileStream);
-      job.setPageable(new PDFPageable(document));
-      job.setPrintService(printService);
-      job.print();
-    } catch (PrinterException | IOException ex) {
-      ex.printStackTrace();
-    }
+    printService.printPDFFile(Paths.get(REPORT_FILE_DEST));
+    Files.delete(Paths.get(REPORT_FILE_DEST));
   }
 }
---
+++ src/main/java/com/timadair/cellphone/service/PrintService.java
@@ -0,0 +1,30 @@
+package com.timadair.cellphone.service;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.printing.PDFPageable;
+
+import javax.print.PrintServiceLookup;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+import static java.nio.file.Files.newInputStream;
+
+public class PrintService {
+  public void printPDFFile(Path samplePdfPath) {
+    javax.print.PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
+    PrinterJob job = PrinterJob.getPrinterJob();
+
+    try (InputStream fileStream = newInputStream(samplePdfPath, StandardOpenOption.READ)) {
+      PDDocument document = PDDocument.load(fileStream);
+      job.setPageable(new PDFPageable(document));
+      job.setPrintService(printService);
+      job.print();
+    } catch (PrinterException | IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
