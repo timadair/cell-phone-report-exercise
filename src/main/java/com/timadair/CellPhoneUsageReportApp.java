7bc54a064e80f876b9c702a8b4a7d749388fe10b
Tim Adair <tymmo.t@gmail.com>
2021-07-31 11:12:33 -0600

    Initial code commit
    Create Maven Project
    Create main class
    Populate .gitignore

---
+++ src/main/java/com/timadair/CellPhoneUsageReportApp.java
@@ -0,0 +1,7 @@
+package com.timadair;
+
+public class CellPhoneUsageReportApp {
+  public static void main(String[] args) {
+    System.out.println("Running Cell Phone Usage Reporting App");
+  }
+}
